29976f6b6035c54330d5da28615a8205b80a3dc1
Emil Ahlbäck <eahlback@kth.se>
2020-01-31 12:56:16 +0100

    simple movement

---
+++ Assets/Scripts/GroundGen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundGen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Generate();
+    }
+
+    void Generate()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    public float sensitivity;
+    public Camera firstPersonCamera;
+
+    CharacterController characterController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forward = Input.GetAxis("Vertical");
+        float side = Input.GetAxis("Horizontal");
+
+        Vector3 movement = transform.forward * forward + transform.right * side;
+        characterController.Move(movement * movementSpeed);
+
+        float h = sensitivity * Input.GetAxis("Mouse X");
+        float v = sensitivity * -Input.GetAxis("Mouse Y");
+
+        transform.Rotate(0, h, 0);
+        firstPersonCamera.transform.Rotate(v, 0, 0);
+    }
+}
