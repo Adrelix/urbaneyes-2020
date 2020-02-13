94269447c7dcdce5d49aa1c503425a02fae7e5d7
kevin-harrison <kevin@harrisoncentral.com>
2020-02-13 19:06:24 +0100

    Component for modifying building GameObjects

---
+++ Assets/Scripts/BuildingEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingEditor : MonoBehaviour
+{
+    public int levels;
+
+    public void UpdateBuilding()
+    {
+        Mesh buildingMesh = GetComponent<MeshFilter>().sharedMesh;
+        Vector3[] vertices = buildingMesh.vertices;
+
+        int numRoofVertices = 4;
+        const float FLOOR_HEIGHT = 5;
+        for(int i = 0; i < numRoofVertices; i++)
+        {
+            vertices[i].y = FLOOR_HEIGHT * levels;
+        }
+        buildingMesh.vertices = vertices;
+        buildingMesh.RecalculateBounds();
+    }
+}
