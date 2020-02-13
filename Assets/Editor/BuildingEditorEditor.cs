d9ce5d5f20399bb70d2705d31a63740f216448e7
kevin-harrison <kevin@harrisoncentral.com>
2020-02-13 19:06:53 +0100

    Added a parser for raw OSM data

---
+++ Assets/Editor/BuildingEditorEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+// Defines the inspector menu of an ObjectBuilder component
+// Unity requires this to be at the path "Assets/Editor"
+// Allows ObjectBuilder.BuildObject() to create GameObjects directly in the scene
+
+[CustomEditor(typeof(BuildingEditor))]
+public class BuildingEditorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        BuildingEditor buildingScript = (BuildingEditor)target;
+        if(GUILayout.Button("Update Building"))
+        {
+            buildingScript.UpdateBuilding();
+        }
+    }
+}
