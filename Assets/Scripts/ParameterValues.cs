467616788030a886dae3700bccc73098a87e641c
adammel <adammel@kth.se>
2020-03-01 20:44:24 +0100

    Added start menu with working parameters that stores information in a static script

---
+++ Assets/Scripts/ParameterValues.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ParameterValues
+{
+    public static float option1, option2, option3, option4, option5;
+
+
+    public static float Option1 {			//note caps på func namn
+	get{return option1;}
+	set{option1=value;}
+    }
+    public static float Option2 {
+	get{return option2;}
+	set{option2=value;}
+    }
+    public static float Option3 {
+	get{return option3;}
+	set{option3=value;}
+    }
+    public static float Option4 {
+	get{return option4;}
+	set{option4=value;}
+    }
+    public static float Option5 {
+	get{return option5;}
+	set{option5=value;}
+    }
+}
---
+++ Assets/Scripts/StartMenuScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class StartMenuScript : MonoBehaviour
+{
+
+
+    public void PlayGame()
+    {
+    SceneManager.LoadScene("SampleScene");
+    }
+
+
+
+    public void AdjustOption1(float newPar){		//Setting the parameter values to the static values that can later be accessed from all scenes	
+    ParameterValues.option1 = newPar;		
+    }
+    public void AdjustOption2(float newPar){
+    ParameterValues.option2 = newPar;
+    }
+    public void AdjustOption3(float newPar){
+    ParameterValues.option3 = newPar;
+    }
+    public void AdjustOption4(float newPar){
+    ParameterValues.option4 = newPar;
+    }
+    public void AdjustOption5(float newPar){
+    ParameterValues.option5 = newPar;
+    }
+
+}
