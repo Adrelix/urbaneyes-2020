0b26c92c00bcb16515427166af58b47800ac781e
Emil Ahlbäck <emil.ahlback99@gmail.com>
2020-03-23 21:34:12 +0100

    Window placement Done by Kevin and Emil

--- Assets/Scripts/Floor.cs
+++ Assets/Scripts/Floor.cs
@@ -16,12 +16,13 @@ public class Floor : MonoBehaviour
         for (int i = 0; i < numWalls; i++) {
             // Create wall vertices
             // Each face of the polygon must not share vertices with the other faces in order
-            // for shaders to consider them as seperate faces and draw sharp edges between them
+            // for shaders to consider them as seperate faces and draw sharp edges between them.
+            // Consider the bottom left vertex of the wall to be the origin of the shape
             Vector3[] vertices = {
-                roofVertices[i],
-                i == numWalls-1 ? roofVertices[0] : roofVertices[i+1], // If last iteration connect with beginning of polygon
-                i == numWalls-1 ? baseVertices[0] : baseVertices[i+1],
-                baseVertices[i]
+                roofVertices[i] - baseVertices[i],
+                i == numWalls-1 ? roofVertices[0] - baseVertices[i] : roofVertices[i+1] - baseVertices[i], // If last iteration connect with beginning of polygon
+                i == numWalls-1 ? baseVertices[0] - baseVertices[i] : baseVertices[i+1] - baseVertices[i],
+                Vector3.zero
             };
 
             // Generate wall triangles
@@ -38,8 +39,9 @@ public class Floor : MonoBehaviour
             mesh.vertices = vertices;
             mesh.triangles = triangles;
 
-            // Generate the wall
-            generateWall(i, mesh, position, material);
+            // Since the vertices are defined at origin, we need to offset the pos by the given position
+            Vector3 newPosition = baseVertices[i] + position;
+            generateWall(i, mesh, newPosition, material);
         }
             
     }
--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Wall : MonoBehaviour {
+public class Wall : MonoBehaviour
+{
 
     public void initWall(Mesh mesh, Vector3 position, Material material) {
         // For lighting
@@ -17,24 +18,55 @@ public class Wall : MonoBehaviour {
         // Set up game object with mesh;
         this.gameObject.AddComponent<MeshFilter>();
         this.gameObject.AddComponent<MeshRenderer>();
+        this.gameObject.AddComponent<MeshCollider>();
         this.gameObject.GetComponent<Transform>().position = position;
         this.gameObject.GetComponent<MeshFilter>().mesh = mesh;
         this.gameObject.GetComponent<MeshRenderer>().material = material;
 
         MeshRenderer renderer = this.gameObject.GetComponent<MeshRenderer>();
         Material tempMaterial = new Material(renderer.sharedMaterial);
-        
+
         // Scale texture according to width and height of the wall
-        // TODO: Remove arbitrary scalar (5f)
         float x = mesh.bounds.size.x;
         float z = mesh.bounds.size.z;
-        float scaleXZ = Mathf.Sqrt((x * x) + (z * z)) / 5f;
-        float scaleY = mesh.bounds.size.y / 5f;
+        float wallLength = Mathf.Sqrt((x * x) + (z * z));
+        float wallHeight = mesh.bounds.size.y;
 
-        tempMaterial.mainTextureScale = new Vector2(scaleXZ, scaleY);
+        // TODO: Remove arbitrary scalar for brick size (5f)
+        tempMaterial.mainTextureScale = new Vector2(wallLength / 5f, wallHeight / 5f);
         renderer.sharedMaterial = tempMaterial;
+        
+        // TODO: get acutal window dimensions
+        float windowLength = 2f;
+        float windowHeight = 1.5f;
+        // Offset is the distance from the corner of the wall to the first window
+        float minOffset = 2f; 
 
-        this.gameObject.AddComponent<MeshCollider>();  
+        // Calculate the maximum num. of windows you can fit on the wall
+        int numOfWindows = (int)((wallLength - (2f * minOffset)) / windowLength);
+        // Calculate the actual margins
+        float offset = (wallLength - (numOfWindows * windowLength)) / 2f;
+        // Calculate direction along the wall
+        Vector3 wallNormal = mesh.normals[0];
+        Vector3 wallPath = Quaternion.AngleAxis(-90f, Vector3.up) * wallNormal;
+
+        Vector3 windowPosition = position + offset * wallPath;
+        windowPosition.y += (wallHeight - windowHeight) / 2; // place windows in center of wall (y-axis)
+        for (int i = 0; i < numOfWindows; i++) {
+            // Center window, x/y-axis
+            windowPosition += wallPath * windowLength / 2f;
+
+            // Load window prefab and set as child
+            GameObject windowInst = Instantiate(Resources.Load("Window_low_poly"), windowPosition, Quaternion.identity) as GameObject;
+            windowInst.transform.parent = this.transform;
+
+            // Rotate window to be parallel with wall
+            windowInst.transform.rotation = Quaternion.FromToRotation(Vector3.forward, wallNormal);
+
+            // Add second half
+            windowPosition += wallPath * windowLength / 2f;
+        }
+
     }
 
     // Return uv mapping relative to a rectangle (our wall)
